06a2ba12e8ee49574ce798b9308ad088f61a0594
Tiago Bryan Ramos de Oliveira <101288219+TiagoBryan@users.noreply.github.com>
2025-09-19 20:46:35 -0300

    Update build.gradle.kts

--- app-basic-kotlin-main/app/build.gradle.kts
+++ app-basic-kotlin-main/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -46,6 +47,7 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
+    implementation(libs.firebase.auth)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
